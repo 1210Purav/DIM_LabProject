0c27215f1adfd47f1373a39530aef5eef2d1fa6e
Purav Shah <113831575+1210Purav@users.noreply.github.com>
2025-08-08 12:36:11 +0530

    Update booking.php

--- booking.php
+++ booking.php
@@ -121,6 +121,15 @@ $prefill_email = $_COOKIE["user_email"] ?? ($_POST["email"] ?? "");
         }
     </style>
 </head>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2EC9KMBLMD"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2EC9KMBLMD');
+</script>
 <body>
 
 <header>
